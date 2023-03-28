be7216f77b3e8623ab5e15b620c2532c11fd3e05
Andre Cadena <al19760606@ite.edu.mx>
2023-03-28 11:24:25 -0700

    Base

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const rutas = [
     {
@@ -24,8 +25,9 @@ const rutas = [
 const NavBar = () => {
   return (
     <nav>
+        
         <ul className='flex flex-row bg-red-500 text-white font-bold text-1xl p-5 justify-evenly'>
-            
+        <Image src='/app/img/logo_metro.png' alt='logo' width={40} height={30}></Image> 
             {rutas.map(({ruta, label}) =>(
                 <li key={ruta}>
                      <Link href={ruta}>{label}</Link>
